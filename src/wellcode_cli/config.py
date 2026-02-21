d6237143f407095c48d38fe3b454077f05835e2b
Gio M <git@giomoros.ca>
2026-02-21 14:09:20 -0700

    Added JIRA implementation

--- src/wellcode_cli/config.py
+++ src/wellcode_cli/config.py
@@ -46,3 +46,15 @@ def get_anthropic_api_key() -> Optional[str]:
 
 def get_split_api_key() -> Optional[str]:
     return get_config_value("SPLIT_API_KEY")
+
+
+def get_jira_url() -> Optional[str]:
+    return get_config_value("JIRA_URL")
+
+
+def get_jira_email() -> Optional[str]:
+    return get_config_value("JIRA_EMAIL")
+
+
+def get_jira_api_token() -> Optional[str]:
+    return get_config_value("JIRA_API_TOKEN")
